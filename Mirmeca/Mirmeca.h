36aa5f25d4bbe20529a8cd88dfedb5426e44268b
solal <solal.fitoussi@gmail.com>
2015-08-01 16:53:21 +1000

    Initial Commit

---
+++ Mirmeca/Mirmeca.h
@@ -0,0 +1,19 @@
+//
+//  Mirmeca.h
+//  Mirmeca
+//
+//  Created by solal on 1/08/2015.
+//  Copyright (c) 2015 Mirmeca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mirmeca.
+FOUNDATION_EXPORT double MirmecaVersionNumber;
+
+//! Project version string for Mirmeca.
+FOUNDATION_EXPORT const unsigned char MirmecaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mirmeca/PublicHeader.h>
+
+
