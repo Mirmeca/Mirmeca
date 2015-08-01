f438d4f405a43c22613dd7578bf56b5fe24e589b
solal <solal.fitoussi@gmail.com>
2015-08-01 18:25:09 +1000

    Creating a first shot of the Comment model
    - The model doesn't not yet contain other WordPress types.
    - Setting up the properties for the model and the necessary methods required by the `Mapper` protocol.
    - Creating the test class that goes with the model to test all properties.
    - Adding a sample JSON file to have a representation for a `Comment` for the tests.

---
+++ Mirmeca/Comment.swift
@@ -0,0 +1,39 @@
+//
+//  Comment.swift
+//  Mirmeca
+//
+//  Created by solal on 1/08/2015.
+//  Copyright (c) 2015 Mirmeca. All rights reserved.
+//
+
+import ObjectMapper
+
+public class Comment: Mappable {
+    
+    public var id: Int?
+    public var post: Int?
+    public var content: String?
+    public var status: String?
+    public var type: String?
+    public var parent: Int?
+    public var date: String?
+    public var dateTz: String?
+    public var dateGmt: String?
+    
+    public class func newInstance() -> Mappable {
+        return Comment()
+    }
+    
+    public func mapping(map: Map) {
+        id      <- map["ID"]
+        post    <- map["post"]
+        content <- map["content"]
+        status  <- map["status"]
+        type    <- map["type"]
+        parent  <- map["parent"]
+        date    <- map["date"]
+        dateTz  <- map["date_tz"]
+        dateGmt <- map["date_gmt"]
+    }
+    
+}
---
+++ MirmecaTests/CommentTestCase.swift
@@ -0,0 +1,67 @@
+//
+//  CommentTestCase.swift
+//  Mirmeca
+//
+//  Created by solal on 1/08/2015.
+//  Copyright (c) 2015 Mirmeca. All rights reserved.
+//
+
+import UIKit
+import XCTest
+import ObjectMapper
+
+class CommentTestCase: XCTestCase {
+    
+    func comment() -> Comment {
+        let path = NSBundle(identifier: "com.Mirmeca")!.pathForResource("comment", ofType: "json")
+        let json = NSString(contentsOfFile: path!, encoding: NSUTF8StringEncoding, error: nil)!
+        return Mapper<Comment>().map(json)!
+    }
+    
+    func testID() {
+        let comment = self.comment()
+        XCTAssertEqual(comment.id!, 1, "Wrong ID")
+    }
+    
+    func testPost() {
+        let comment = self.comment()
+        XCTAssertEqual(comment.post!, 1, "Wrong Post")
+    }
+    
+    func testContent() {
+        let comment = self.comment()
+        XCTAssertEqual(comment.content!, "<p>Hi, this is a comment.<br />\nTo delete a comment, just log in and view the post&#039;s comments. There you will have the option to edit or delete them.</p>\n", "Wrong Content")
+    }
+    
+    func testStatus() {
+        let comment = self.comment()
+        XCTAssertEqual(comment.status!, "approved", "Wrong Status")
+    }
+    
+    func testType() {
+        let comment = self.comment()
+        XCTAssertEqual(comment.type!, "comment", "Wrong Type")
+    }
+    
+    func testParent() {
+        let comment = self.comment()
+        XCTAssertEqual(comment.parent!, 0, "Wrong Parent")
+    }
+    
+    func testDate() {
+        let comment = self.comment()
+        XCTAssertEqual(comment.date!, "2015-07-27T10:50:05", "Wrong Date")
+    }
+    
+    func testDateTz() {
+        let comment = self.comment()
+        XCTAssertEqual(comment.dateTz!, "UTC", "Wrong DateTz")
+    }
+    
+    func testDateGmt() {
+        let comment = self.comment()
+        XCTAssertEqual(comment.dateGmt!, "2015-07-27T10:50:05", "Wrong DateGmt")
+    }
+    
+}
+
