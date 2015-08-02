e4daa85871e9b33b8e59c64b9a21a616c512cb57
solal <solal.fitoussi@gmail.com>
2015-08-02 17:31:24 +1000

    Adding the Gateway Protocol
    - The protocol is to be implemented by all gateways.

---
+++ Mirmeca/GatewayProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  GatewayProtocol.swift
+//  Mirmeca
+//
+//  Created by solal on 2/08/2015.
+//  Copyright (c) 2015 Mirmeca. All rights reserved.
+//
+
+import Foundation
+
+protocol GatewayProtocol {
+    
+    init(endpoint: String, env: String?)
+    func request(completion: (value: AnyObject?, error: NSError?) -> Void)
+    
+}
